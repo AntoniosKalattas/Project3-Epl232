134e905e309f4bf4b67602d7e16cda1f6fd7f588
Alexandros Georgiou <166490546+alexcodes16@users.noreply.github.com>
2024-11-07 10:45:38 +0200

    updated main
    updated main with some additions/removals to the comments

--- main.c
+++ main.c
@@ -6,9 +6,8 @@
 
 /** @struct BackTrackingCounters
  * 
- * This struct is keeping track, on the counters, such
- * as how many pushes, and pops, and the total steps;
- * 
+ * struct that keeps track, on the counters, such
+ * as how many pushes, pops, and total steps
  */
 typedef struct{
     int step;   
@@ -16,19 +15,19 @@ typedef struct{
     int pops;
 }BackTrackingCounters;
 
-/** @brief It will initialise the BackTrackingCounters with default values.
+/** @brief initializes the BackTrackingCounters with the default values
  * 
- * @param BackTrackingCounters Pointer to BackTrackingCounters.
- * @return void.
+ * @param BackTrackingCounters Pointer to BackTrackingCounters
+ * @return void
  */
 void initBackTrackingCounters(BackTrackingCounters *counters);
 
-/** @brief It will solve the given LatinSquare.
+/** @brief solves the given LatinSquare
  * 
- * Using recursion and backtracking it will solve the latinSquare.
- * It will move forward if the algorithm can find a number to place on the 
- * first empty slot. If it cannot, it will go to the previous empty slot
- * and try to find a new value to assing there.
+ * Using recursion and backtracking it solves the latinSquare by moving
+ * forward if the algorithm finds a number to place on the 
+ * first empty slot. If it cannot, it backtracks to the previous empty slot
+ * and tries to find a new value to assign there
  * 
  * @param Node Pointer currect top node of the stack.
  * @param int The size of the latinSquare.
@@ -40,30 +39,30 @@ void initBackTrackingCounters(BackTrackingCounters *counters);
 void solveLatinSquare(Node *node, int size, Stack *stack, BackTrackingCounters *);
 
 int main(int argc, char * argv[]){
-    if(argc==1){                                        // if given file is null.
+    if(argc==1){                                        // file's null.
         perror("Please give a file name.");
         return EXIT_FAILURE;
     }
-    int **latinSquare=NULL;                             // 2d array to store the first state puzzle.
+    int **latinSquare=NULL;                             // 2d array to store the first state puzzle
 
-    int size = readLatinSquare(&latinSquare, argv[1]);  // size variable.
+    int size = readLatinSquare(&latinSquare, argv[1]);  // size variable
 
-    Stack *stack = (Stack *)malloc(sizeof(Stack));      // create a stack, used for storing the states of the latinSquares.
-    if(stack==NULL){                                    // if allocation faild.
+    Stack *stack = (Stack *)malloc(sizeof(Stack));      // creates a stack, used for storing the states of the latinSquares
+    if(stack==NULL){                                    // allocation fails
         printf("Faild to allocate space for stack.");
         return EXIT_FAILURE;
     }
-    initStack(stack);                                   // initiallise the stack.
+    initStack(stack);                                   // initiallizes the stack
 
-    push(stack,latinSquare,size,0,0);                   // push the very first state of the latin square.
-    BackTrackingCounters *counters =(BackTrackingCounters *)malloc(sizeof(BackTrackingCounters));   // allocate space for BackTrackingCounters.
-    if(counters==NULL){                                 // if allocation faild.
+    push(stack,latinSquare,size,0,0);                   // pushes the very first state of the latin square
+    BackTrackingCounters *counters =(BackTrackingCounters *)malloc(sizeof(BackTrackingCounters));   // allocates space for BackTrackingCounters
+    if(counters==NULL){                                 // if allocation fails
         printf("Faild to allocate space for BackTrackingCounters.");
         return EXIT_FAILURE;
     }
-    initBackTrackingCounters(counters);                 // initialise the BackTrackingCounters.
+    initBackTrackingCounters(counters);                 // initializes the BackTrackingCounters
 
-    solveLatinSquare(stack->top, size, stack, counters);// Solve the latinSquare.
+    solveLatinSquare(stack->top, size, stack, counters);// solves the latinSquare
     printf("PUSH NUM: %d\n",counters->pushes);
     printf("POP NUM: %d",counters->pops); 
     freeStack(stack, size);
@@ -74,34 +73,34 @@ void solveLatinSquare(Node *node, int size, Stack *stack, BackTrackingCounters *
     if(isSolved(node->square, size))
         return;
     
-    int position = findEmptySlot(node->square,size);        // position that we can enter a number.
+    int position = findEmptySlot(node->square,size);        // position that we can enter a number (the empty spot)
     bool found=false;                                       // variable to know if a number was found to fit in the slot.
-    for(int number=1;number<=size;number++){                // try all the possible numbers, from 1 to size.
-        if(checkMove(node->square,size,number,position)){   // cehck if the random number can be placed there.
+    for(int number=1;number<=size;number++){                // tries all the possible numbers, from 1 to size
+        if(checkMove(node->square,size,number,position)){   // checks if the random number can be placed there
             found=true;
             int row =(position)%size;                       
             int col =(position)/size;
-            insertNumber(&node->square,size,position,number);   // insert the number in the latinSquare.
-            counter->pushes++;                                  // increase the push counter.
-            push(stack,node->square,size,row,col);              // push the new state of the latinSquare.
+            insertNumber(&node->square,size,position,number);   // inserts the number in the latinSquare
+            counter->pushes++;                                  // increases the push counter
+            push(stack,node->square,size,row,col);              // pushes the new state of the latinSquare
             printf("PUSH: STEP %d\n", counter->step);              
-            printLatinSquare(&node->square,size);               // prints the state of the latin Square.
-            counter->step++;                                    // increase the pop counter.
-            solveLatinSquare(stack->top,size,stack, counter);   // recursive call.                                        
-            if(isSolved(stack->top->square, size))              // if the recursive call back is because the puzzle is solved.
+            printLatinSquare(&node->square,size);               // prints the state of the latin Square
+            counter->step++;                                    // increases the pop counter
+            solveLatinSquare(stack->top,size,stack, counter);   // recursive call in order to solve the latinSquare, by keep adding numbers                                        
+            if(isSolved(stack->top->square, size))              // recursive calls back; puzzle is solved
                 return;
-            else{                                               // if the recursive call back was due to backtracking.
-                found=false;                                    // make the found variable false again.
-                Node *n = pop(stack);                           // pop the very top element in the stack -> delete the previous state of the latinSquare.
+            else{                                               // recursive calls back for backtracking
+                found=false;                                    // makes the found variable false again (since it wasn't a match)
+                Node *n = pop(stack);                           // pops the very top element in the stack -> deletes the previous state of the latinSquare.
                 freeNode(n,size);
                 printf("POP: STEP %d\n", counter->step);
                 printLatinSquare(&stack->top->square,size);
-                counter->pops++;                                // increase the pop counter.
-                counter->step++;                                // increase the total steps.
+                counter->pops++;                                // increases the pop counter
+                counter->step++;                                // increases the total steps
             }
         }
     }
-    if(!found){                                                 // if we couldn't find a number for that slot, backtrack to previous state.
+    if(!found){                                                 // no number found for that slot; backtracks to previous state.
         return;
     }
 }
