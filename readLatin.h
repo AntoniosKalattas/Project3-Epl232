84da97c87766b2636d4554a85c3fc1ba34a2df83
Alexandros Georgiou <166490546+alexcodes16@users.noreply.github.com>
2024-11-07 12:15:44 +0200

    Updated readLatin.h with comments

--- readLatin.h
+++ readLatin.h
@@ -1,6 +1,24 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+/**
+ * @brief reads a Latin square from a file and initializes with its values
+ * 
+ * This function opens the file and reads the size and values of the Latin square, 
+ * dynamically allocating memory for the 2D array to store these values
+ * 
+ * @param latinSquare Pointer to a pointer of a 2D array that will hold the Latin square values
+ * @param filename Name of the file containing the Latin square data
+ * @return int Size of the Latin square
+ */
 int readLatinSquare(int ***, char *);
 
+/**
+ * @brief prints the Latin square in a formatted layout
+ * 
+ * This function outputs the current state of the Latin square
+ * 
+ * @param latinSquare Pointer to a 2D array representing the Latin square
+ * @param size Size of the Latin square (n x n)
+ */
 void printLatinSquare(int ***, int);
